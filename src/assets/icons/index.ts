985d022f2e9cd576f35e05aa5214940d783165e1
OH <almighty5@snu.ac.kr>
2025-11-07 23:59:05 +0900

    feat: implement header

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import Home from "../icons/Home.svg";
+import Logo from "../icons/logo.svg";
+import Profile from "../icons/Profile.svg";
+
+export { Home, Logo, Profile };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+  const showHeader = pathname !== "/";
+
   return (
     <div className="w-full min-h-screen bg-bg-default">
-      <Header />
+      {showHeader && <Header />}
       <main className="mx-auto w-full max-w-4xl px-12 py-4">
         <Outlet />
       </main>
