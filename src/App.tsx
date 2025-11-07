2e96db59aae510d565eaeac443ff04439e4102be
sumin <smkl1004@snu.ac.kr>
2025-11-07 22:38:50 +0900

    feat: create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
     <>
       <h1 className="font-bold text-red-500">Vite + React</h1>
+      <Button text="Hello Button" />
     </>
   );
 }
